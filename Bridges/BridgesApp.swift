520129e8133b9091deb0f432992cc9155dd8b387
ZaidiHamza <hamzaz012803@gmail.com>
2025-03-11 01:45:03 +0000

    Initial Commit

---
+++ Bridges/BridgesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BridgesApp.swift
+//  Bridges
+//
+//  Created by Hamza Zaidi on 11/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct BridgesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BridgesTests/BridgesTests.swift
@@ -0,0 +1,17 @@
+//
+//  BridgesTests.swift
+//  BridgesTests
+//
+//  Created by Hamza Zaidi on 11/03/2025.
+//
+
+import Testing
+@testable import Bridges
+
+struct BridgesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
